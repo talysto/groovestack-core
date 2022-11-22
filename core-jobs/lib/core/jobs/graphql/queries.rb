fdfca729d76e806aa66600136f6e488dbef13872
Max Schridde <maxjschridde@gmail.com>
2022-11-22 10:50:18 -0800

    fix ID type

--- core-jobs/lib/core/jobs/graphql/queries.rb
+++ core-jobs/lib/core/jobs/graphql/queries.rb
@@ -9,7 +9,7 @@ module Core
             # resource 'core/jobs/job', entity: :jobs
 
             field :Job, ::Core::Jobs::GraphQL::Types::Job, null: true, resolver_method: :job do
-              argument :id, ::GraphQL::Types::Base::Object::ID, required: true
+              argument :id, ID, required: true
             end
 
             field :allJobs, [::Core::Jobs::GraphQL::Types::Job], null: false, resolver_method: :jobs do
