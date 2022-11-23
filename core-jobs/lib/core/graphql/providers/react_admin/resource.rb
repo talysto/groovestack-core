2f1e35495834f2343edd03e09424760fdeaa4eca
Max Schridde <maxjschridde@gmail.com>
2022-11-22 17:06:05 -0800

    specify type within react admin provider resource helper for collection queries

--- core-jobs/lib/core/graphql/providers/react_admin/resource.rb
+++ core-jobs/lib/core/graphql/providers/react_admin/resource.rb
@@ -58,7 +58,7 @@ module Core
               # Collection
               
               unless except.include?(:collection)
-                ::GraphQL::Schema::Object.field "all#{entity.to_s.camelize}".to_sym, [entity_type], null: false, resolver_method: entity do
+                ::GraphQL::Schema::Object.field "all#{entity.to_s.camelize}".to_sym, type: [entity_type], null: false, resolver_method: entity do
                   argument :page, ::GraphQL::Types::Int, required: false
                   argument :per_page, ::GraphQL::Types::Int, required: false
                   argument :sort_field, ::GraphQL::Types::String, required: false
@@ -70,7 +70,7 @@ module Core
               # Collection meta
   
               unless except.include?(:collection_meta)
-                ::GraphQL::Schema::Object.field "_all#{entity.to_s.camelize}Meta".to_sym, ::Core::GraphQL::Providers::ReactAdmin::Types::RAListMetadata, camelize: false, null: true, resolver_method: "#{entity}_meta".to_sym do
+                ::GraphQL::Schema::Object.field "_all#{entity.to_s.camelize}Meta".to_sym, type: ::Core::GraphQL::Providers::ReactAdmin::Types::RAListMetadata, camelize: false, null: true, resolver_method: "#{entity}_meta".to_sym do
                   argument :page, ::GraphQL::Types::Int, required: false
                   argument :per_page, ::GraphQL::Types::Int, required: false
                   argument :sort_field, ::GraphQL::Types::String, required: false
