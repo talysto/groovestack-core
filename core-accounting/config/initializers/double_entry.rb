6097f92a301f89a3683a89e3cea0731e1318cd70
Max Schridde <maxjschridde@gmail.com>
2023-02-03 14:42:08 -0800

    remove unnecessary json_metadata config var

--- core-accounting/config/initializers/double_entry.rb
+++ core-accounting/config/initializers/double_entry.rb
@@ -1,10 +1,5 @@
 require 'double_entry'
 
-DoubleEntry.configure do |config|
-  # Use json(b) column in double_entry_lines table to store metadata instead of separate metadata table
-  config.json_metadata = true
-end
-
 DoubleEntry::Line.class_eval do
   scope :by_scope_id, ->(id) { where(scope: id) }
   scope :by_partner_scope_id, ->(partner_id) { where(partner_scope: partner_id) }
