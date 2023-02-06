1e0c45a3e87867d6d08be648e866e760b49dfa0d
Max Schridde <maxjschridde@gmail.com>
2023-02-06 15:26:14 -0800

    increase scope and account identifier max lengths (core-accounting)

--- core-accounting/config/initializers/double_entry.rb
+++ core-accounting/config/initializers/double_entry.rb
@@ -1,5 +1,10 @@
 require 'double_entry'
 
+DoubleEntry.configure do |config|
+  config.scope_identifier_max_length = 1024
+  config.account_identifier_max_length = 1024
+end
+
 DoubleEntry::Line.class_eval do
   scope :by_scope_id, ->(id) { where(scope: id) }
   scope :by_partner_scope_id, ->(partner_id) { where(partner_scope: partner_id) }
