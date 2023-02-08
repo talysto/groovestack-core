6990eb3ad61fa8a510f7640ec604da6675d371c2
Max Schridde <maxjschridde@gmail.com>
2023-02-08 11:58:21 -0800

    add scopes to double entry line base class

--- core-accounting/config/initializers/double_entry.rb
+++ core-accounting/config/initializers/double_entry.rb
@@ -5,3 +5,10 @@ DoubleEntry.configure do |config|
   config.scope_identifier_max_length = 1024
   config.account_identifier_max_length = 1024
 end
+
+module DoubleEntry
+  class Line < ActiveRecord::Base
+    scope :credits, -> { where('amount > 0') }
+    scope :debits, -> { where('amount < 0') }
+  end
+end
