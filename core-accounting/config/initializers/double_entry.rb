2810652414595800b426cd61e8a2034110777b32
Max Schridde <maxjschridde@gmail.com>
2023-02-08 10:51:26 -0800

    specify core-accounting DE config linemetadata as json

--- core-accounting/config/initializers/double_entry.rb
+++ core-accounting/config/initializers/double_entry.rb
@@ -1,6 +1,7 @@
 require 'double_entry'
 
 DoubleEntry.configure do |config|
+  config.json_metadata = true
   config.scope_identifier_max_length = 1024
   config.account_identifier_max_length = 1024
 end
