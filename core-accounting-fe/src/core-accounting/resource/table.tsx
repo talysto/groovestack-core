3f939f9ac4259a2c0ed0043df24cea0e04bab18e
Max Schridde <maxjschridde@gmail.com>
2023-02-13 09:54:08 -0800

    core-accounting-fe improvements

--- core-accounting-fe/src/core-accounting/resource/table.tsx
+++ core-accounting-fe/src/core-accounting/resource/table.tsx
@@ -1,19 +1,30 @@
 import React from 'react'
 
-import { List, Datagrid, TextField, NumberField} from 'react-admin'
+import { List, Datagrid, TextField, NumberField, useRecordContext} from 'react-admin'
 
 import { TimeAgoField } from './TimeAgoField'
 
-export const Table = () => {
+const MoneyField: React.FC<{source: string}> = ({source}) => {
+  const record = useRecordContext()
+
+  if (!record) return null
+
+  return <span>{record[source].formatted_amount} {record[source].currency.code}</span>
+}
+
+export const Table: React.FC<{datagridProps?: any; tableProps?: any, children?: any}> = ({datagridProps, tableProps, children}) => {
   return (
-    <List exporter={false}>
-      <Datagrid sort={{ field: 'createdAt', order: 'DSC' }} rowClick="show">
-        <TextField source="id" sortable={false} />
-        <TextField source="account" sortable={false} />
-        <TextField source="scope" sortable={false} />
+    <List exporter={false} {...tableProps}>
+      <Datagrid sort={{ field: 'createdAt', order: 'DESC' }} rowClick="show">
+        {/* <TextField source="id" sortable={false} /> */}
+        {/* <TextField source="account" sortable={false} />
+        <TextField source="scope" sortable={false} /> */}
+        {children}
         <TextField source="code" sortable={false} />
-        <NumberField label="Amount" source="amount.formatted_amount" sortable={false} />
-        <NumberField label="Balance" source="balance.formatted_amount" sortable={false} />
+        <MoneyField source="amount" />
+        <MoneyField source="balance" />
+        {/* <NumberField label="Amount" source="amount.formatted_amount" sortable={false} />
+        <NumberField label="Balance" source="balance.formatted_amount" sortable={false} /> */}
         <TimeAgoField label="Created" source="createdAt" />
       </Datagrid>
     </List>
