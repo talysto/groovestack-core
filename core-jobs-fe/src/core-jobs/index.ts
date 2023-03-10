7fb90c2ec577aabf8daf04c61ee4c4df105a97a6
Darren Rush <dlrush@gmail.com>
2023-03-09 16:44:11 -0800

    Export mockJob from the main module

--- core-jobs-fe/src/core-jobs/index.ts
+++ core-jobs-fe/src/core-jobs/index.ts
@@ -1,6 +1,8 @@
 import { Charts } from './views'
 import { Jobs } from './resource'
 
+export { mockJob } from './mock-job'
+
 export class CoreJobs {
   static Charts = Charts
   static Resource = Jobs
