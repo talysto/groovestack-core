d76149dfabbbf14b0644917c176d600f6642093f
Max Schridde <maxjschridde@gmail.com>
2023-03-16 13:01:43 -0700

    test-core-fe app running after merge

--- core-comments-fe/src/core-comments/resources/comments/index.ts
+++ core-comments-fe/src/core-comments/resources/comments/index.ts
@@ -1,11 +1,11 @@
-import CommentOutlinedIcon from '@mui/icons-material/CommentOutlined'
+// import CommentOutlinedIcon from '@mui/icons-material/CommentOutlined'
 
 import { CommentsEdit } from './Edit'
 import { CommentsList } from './List'
 import { CommentsShow } from './Show'
 
 export class Comments {
-  static Icon = CommentOutlinedIcon
+  // static Icon = CommentOutlinedIcon
   static List = CommentsList
   static Edit = CommentsEdit
   static Show = CommentsShow
