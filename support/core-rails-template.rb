083addc853c4f5cedc52c682e738c11e01a64592
Darren Rush <dlrush@gmail.com>
2023-03-16 18:25:06 -0700

    Add a starter rails app template

---
+++ support/core-rails-template.rb
@@ -0,0 +1,23 @@
+# CORE Applications Template
+# USAGE
+# rails new -m https://raw.githubusercontent.com/moonlight-labs/core/main/support/core-rails-template.rb
+
+# Reference: https://guides.rubyonrails.org/rails_application_templates.html
+
+# starter command:
+#  -d postgresql --skip-turbolinks --skip-jbuilder --skip-webpack-install
+
+gem "pg"
+gem "graphql"
+gem "uuid"
+gem "vite_rails"
+
+# generate(:scaffold, "person name:string")
+# route "root to: 'people#index'"
+# rails_command("db:migrate")
+
+# after_bundle do
+#   git :init
+#   git add: "."
+#   git commit: %Q{ -m 'Initial commit' }
+# end
