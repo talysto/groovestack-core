e2f461cc9c01d3238f5b1587abdc50b952a09bf7
Max Schridde <maxjschridde@gmail.com>
2023-03-20 15:57:14 -0700

    [core-accounting-fe] ReferenceManyLines component

---
+++ core-accounting-fe/src/core-accounting/resource/MoneyField.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { useRecordContext} from 'react-admin'
+
+export const MoneyField: React.FC<{source: string}> = ({source}) => {
+  const record = useRecordContext()
+
+  if (!record) return null
+
+  return <span>{record[source].formatted_amount} {record[source].currency.code}</span>
+}
---
+++ core-accounting-fe/src/core-accounting/resource/ReferenceManyLines.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react"
+import { ReferenceManyField, TextField, Datagrid } from "react-admin"
+import { MoneyField } from "./MoneyField"
+import { TimeAgoField } from "./TimeAgoField"
+
+export const ReferenceManyLines: FC<{children?: any}> = ({children}) => {
+  return (
+    <ReferenceManyField
+      reference="Line"
+      target="scope"
+    >
+      <Datagrid bulkActionButtons={false}>
+        <TextField source="code" sortable={false} />
+        <MoneyField source="amount" />
+        <MoneyField source="balance" />
+        {...children}
+        <TimeAgoField label="Created" source="createdAt" />
+      </Datagrid>
+    </ReferenceManyField>
+  )
+}
