e2c4c151729133600cdf403e2e49c96950d7274c
Max Schridde <maxjschridde@gmail.com>
2023-03-20 15:59:29 -0700

    [core-accounting] config.account_scope_identifier_mapping

--- core-accounting/config/initializers/double_entry.rb
+++ core-accounting/config/initializers/double_entry.rb
@@ -1,5 +1,20 @@
 require 'double_entry'
 
+module DoubleEntry
+  class Configuration 
+    # intended as a hash mapping for account scopes to model names
+    # ex: 
+    #   config.account_scope_identifier_mapping = {
+    #     aqd_treasury: 'OrgUnit',
+    #     aqd_tokens: 'User'
+    #   }
+    # AFAICT scope identifier classes can't be pulled directly 
+    # from account.scope_identifier
+    
+    attr_accessor :account_scope_identifier_mapping
+  end
+end
+
 DoubleEntry.configure do |config|
   config.json_metadata = true
   config.scope_identifier_max_length = 1024
