4439b10717194c62166017ee20c58b589ba191b3
David <0davidcaplin@gmail.com>
2023-03-22 19:20:58 -0700

    WIP -  mock transfers

--- core-accounting-fe/src/core-accounting/mockLines.ts
+++ core-accounting-fe/src/core-accounting/mockLines.ts
@@ -8,18 +8,46 @@ Array.prototype.sample = function () {
   return this[Math.floor(Math.random() * this.length)]
 }
 
+
 // TO DO: finish fixing data + 2nd pass 
-export async function mockTransfers({count = 8}): Promise<any> {
+export async function mockTransfers({ count = 8 }): Promise<any> {
   try {
     const { faker } = await import('@faker-js/faker')
-    return Array.from({length: count}, () => ({
+    return Array.from({ length: count }, () => ({
       // standard attributes
-      id: faker.datatype.uuid(),
-      account: faker.datatype.uuid(),
+     
+      account: ["aqd_tokens", "aqd_treasury"].sample() //faker.datatype.uuid(),
       scope: faker.datatype.uuid(),
       code: ['buy_aqd', 'spend_aqd', 'transfer_aqd?'].sample(),
+//----------------------
       amount: faker.commerce.price(1, 1500, 0),
       balance: faker.commerce.price(1, 1500, 0),
+
+      detailId: faker.datatype.uuid(),
+      detailType: ['PaymentItem::1', 'PaymentItem::2'].sample(),
+      // TODO improve metadata fake data
+      metadata: {},// { key1: ['value 1', 'value 2'], key2: 'value 3' },
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+//----------------------
+      partnerId: faker.datatype.uuid(),
+      partnerAccount: "aqd_treasury",
+      partnerScope: faker.datatype.uuid(),
+      //   "key1" => [],
+      //   "key2" => nil
+
+      // "amount" => 1500,
+      // "balance" => 1500,
+
+      // "detail_type" => nil,
+      // "detail_id" => nil,
+      // "metadata" => {},
+      // "created_at" => Thu, 16 Mar 2023 23:00:51.747097000 UTC +00:00,
+      // "updated_at" => Thu, 16 Mar 2023 23:00:51.747097000 UTC +00:00,
+      //   "key1" => [],
+      //   "key2" => nil
+
+    
       // amount: {
       //   amount: faker.commerce.price(1, 1500, 0),
       //   formatted_amount: faker.commerce.price(1, 1000, 0, '$'),
@@ -36,17 +64,6 @@ export async function mockTransfers({count = 8}): Promise<any> {
       //     symbol: '$',
       //   },
       // },
-      partnerId: faker.datatype.uuid(),
-      partnerAccount: "aqd_treasury",
-      partnerScope: faker.datatype.uuid(),
-      detailId: faker.datatype.uuid(),
-      detailType: ['PaymentItem::1', 'PaymentItem::2'].sample(),
-      // TODO improve metadata fake data
-      metadata: {},// { key1: ['value 1', 'value 2'], key2: 'value 3' },
-      createdAt: faker.date.recent(),
-      updatedAt: faker.date.recent(),
-
-
     }))
   } catch (e) {
     console.error(e)
@@ -56,7 +73,7 @@ export async function mockTransfers({count = 8}): Promise<any> {
   // 8.times do 
   //   create line 
   //     credit amount
-      
+
   //   create partner line
   //     debit amount
 
@@ -64,11 +81,30 @@ export async function mockTransfers({count = 8}): Promise<any> {
   //   grab a user & a company
   //   line 1 has user as account / scope and company as partner account / scope
   //   line 2 has company as account / scope and user as partner account / scope
+
+
+  //   #<DoubleEntry::Line:0x000000010f3103e8> {
+  //     "id" => "6dd32946-f212-41a8-b9d2-065c89737c1f",
+  // "account" => "aqd_tokens",
+  //  "scope" => "10beb7bc-d420-4eae-afb5-43124044bd25",
+  //   "code" => "buy_aqd",
+  // "amount" => 1500,
+  // "balance" => 1500,
+  // "partner_id" => "b4e6133a-3ba6-46f7-9f98-92238f975f48",
+  // "partner_account" => "aqd_treasury",
+  // "partner_scope" => "7dcdd3c5-1943-424d-bd62-8a214924b996",
+  // "detail_type" => nil,
+  // "detail_id" => nil,
+  // "metadata" => {},
+  // "created_at" => Thu, 16 Mar 2023 23:00:51.747097000 UTC +00:00,
+  // "updated_at" => Thu, 16 Mar 2023 23:00:51.747097000 UTC +00:00,
+  //   "key1" => [],
+  //   "key2" => nil
 }
-export async function mockLines({count = 15}): Promise<any> {
+export async function mockLines({ count = 15 }): Promise<any> {
   try {
     const { faker } = await import('@faker-js/faker')
-    return Array.from({length: count}, () => ({
+    return Array.from({ length: count }, () => ({
       // standard attributes
       id: faker.datatype.uuid(),
       account: faker.datatype.uuid(),
--- test-core-fe/src/data/mock-data-provider.ts
+++ test-core-fe/src/data/mock-data-provider.ts
@@ -11,7 +11,7 @@ import { mockCompanies } from './mockCompanies'
 const users = mockUsers(10)
 const companies = mockCompanies(5)
 
-const comments = (await mockComments({count: 50})).map((comment) => {
+const comments = (await mockComments({ count: 50 })).map((comment) => {
   const user = faker.helpers.arrayElement(users)
   const resource = faker.helpers.arrayElement([
     faker.helpers.arrayElement(users),
@@ -32,7 +32,7 @@ const comments = (await mockComments({count: 50})).map((comment) => {
   }
 })
 
-const versions = (await mockVersions({count: 20})).map((comment) => {
+const versions = (await mockVersions({ count: 20 })).map((version) => {
   const user = faker.helpers.arrayElement(users)
   const resource = faker.helpers.arrayElement([
     faker.helpers.arrayElement(users),
@@ -40,7 +40,7 @@ const versions = (await mockVersions({count: 20})).map((comment) => {
   ])
 
   return {
-    ...comment,
+    ...version,
 
     actor_id: user.id,
     actor_type: user.type,
@@ -53,6 +53,55 @@ const versions = (await mockVersions({count: 20})).map((comment) => {
   }
 })
 
+
+const transfers = (await mockVersions({ count: 20 })).map((transfer) => {
+  const user = faker.helpers.arrayElement(users)
+  const resource = faker.helpers.arrayElement([
+    faker.helpers.arrayElement(users),
+    faker.helpers.arrayElement(companies),
+  ])
+
+  return {
+
+    const line1 = {
+      ...transfer,
+
+      id: user.id,
+      account: "aqd_tokens", //faker.datatype.uuid(),
+      scope: faker.datatype.uuid(),
+      code: ['buy_aqd', 'spend_aqd', 'transfer_aqd?'].sample(),
+
+      partnerId: resource.id,
+      partnerAccount: "aqd_treasury",
+      partnerScope: faker.datatype.uuid(),
+    }
+      const line2 = { //line 2 (FLIP)
+
+      partnerId: line1.id,
+      account: line1.partnerAccount,
+      scope: line1.partnerScope,
+    }
+
+    actor_id: user.id,
+    actor_type: user.type,
+
+    resource_id: resource.id,
+    resource_type: resource.type,
+
+    // author: user,
+    // resource: resource
+  }
+})
+// - Define types of Accounts that exist and scope to user / unit(ie type of bank and a user is the scope)
+//   - And specify the transfers that can exist between accounts
+
+
+
+// for each pair of credit / debit lines
+//   grab a user & a company
+//   line 1 has user as account / scope and company as partner account / scope
+//   line 2 has company as account / scope and user as partner account / scope
+
 export const mockDataProvider = fakeDataProvider(
   {
     // CORE Modules
@@ -60,7 +109,8 @@ export const mockDataProvider = fakeDataProvider(
     lockers: await mockLockers(3),
 
     Comment: comments,
-    Line: await mockLines({count: 20}),
+    Line: await mockLines({ count: 20 }),
+    Transfer: transfers,
     Version: versions,
     // For Testing Integrations
     User: users,
