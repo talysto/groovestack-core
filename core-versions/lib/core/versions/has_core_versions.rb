a5346562e0bc0ee829dd2c75de6e64819f9937c3
adam <adam@moonlightlabs.com>
2023-03-30 15:24:19 -0700

    add set_core_versions_whodunnit

--- core-versions/lib/core/versions/has_core_versions.rb
+++ core-versions/lib/core/versions/has_core_versions.rb
@@ -1,9 +1,13 @@
 module Core
   module Versions
     module HasCoreVersions
-      def has_core_versions(opts)
+      def has_core_versions(opts = {})
         has_paper_trail(opts)
       end
     end
   end
+end
+
+ActiveSupport.on_load(:active_record) do
+  ActiveRecord::Base.send :extend, Core::Versions::HasCoreVersions
 end
