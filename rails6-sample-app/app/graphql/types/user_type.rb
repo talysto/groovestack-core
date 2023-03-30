494701887a714e10fe95464e4e65604dcd02c86f
Max Schridde <maxjschridde@gmail.com>
2023-03-30 10:38:14 -0700

    [core-comments] test commentable type concern in rails6 sample app

--- rails6-sample-app/app/graphql/types/user_type.rb
+++ rails6-sample-app/app/graphql/types/user_type.rb
@@ -1,5 +1,7 @@
 module Types
   class UserType < Types::BaseObject
+    include ::Core::Comments::GraphQL::Types::Commentable
+    
     field :id, ID, null: false
     field :name, String, null: false
     field :email, String, null: false
