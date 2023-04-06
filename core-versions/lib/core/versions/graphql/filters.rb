b5bc78a9c1967ed9cbe744352bc97c0f8fab22df
adam <adam@moonlightlabs.com>
2023-04-06 12:05:57 -0700

    add resource_id and resource_type to version filters

--- core-versions/lib/core/versions/graphql/filters.rb
+++ core-versions/lib/core/versions/graphql/filters.rb
@@ -11,6 +11,8 @@ module Core
           argument :created_at_gte, String, required: false
           argument :item_id, ID, required: false
           argument :item_type, String, required: false
+          argument :resource_id, ID, required: false
+          argument :resource_type, String, required: false
           argument :actor_id, ID, required: false
           argument :actor_type, String, required: false
         end
