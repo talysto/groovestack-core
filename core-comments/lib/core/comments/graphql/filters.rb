e086eb1e3e930883fac91954c9d332212aceff93
Max Schridde <maxjschridde@gmail.com>
2023-04-10 14:09:58 -0700

    [core-comments] FIX filters argument type description references to core-base

--- core-comments/lib/core/comments/graphql/filters.rb
+++ core-comments/lib/core/comments/graphql/filters.rb
@@ -10,9 +10,9 @@ module Core
           argument :author_id, ID, required: false, description: 'id of author relation'
           argument :author_type, String, required: false, description: 'class of author relation'
           argument :created_at_gte, String, required: false, description: ::Core::Base::GraphQL::Documentation::Arguments.created_at_gte
-          argument :created_at_lte, String, required: false, description: ::CoreBase::GraphQL::Documentation::Arguments.created_at_lte
-          argument :ids, [ID], required: false, description: ::CoreBase::GraphQL::Documentation::Arguments.ids
-          argument :q, String, required: false, description: ::CoreBase::GraphQL::Documentation::Arguments.q
+          argument :created_at_lte, String, required: false, description: ::Core::Base::GraphQL::Documentation::Arguments.created_at_lte
+          argument :ids, [ID], required: false, description: ::Core::Base::GraphQL::Documentation::Arguments.ids
+          argument :q, String, required: false, description: ::Core::Base::GraphQL::Documentation::Arguments.q
           argument :resource_id, ID, required: false, description: 'id of resource relation'
           argument :resource_type, String, required: false, description: 'class of resource relation'
         end
