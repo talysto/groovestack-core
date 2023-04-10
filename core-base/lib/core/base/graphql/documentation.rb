babe5d9220d9e51e3dc32359b9f949e54c8684f6
Max Schridde <maxjschridde@gmail.com>
2023-04-10 13:20:50 -0700

    [core-base] WIP graphql documentation module

---
+++ core-base/lib/core/base/graphql/documentation.rb
@@ -0,0 +1,11 @@
+module Core
+  module Base
+    module GraphQL
+      module Documentation
+        Fields = OpenStruct.new({
+          id: 'a unique record identifier'
+        })
+      end
+    end
+  end
+end
