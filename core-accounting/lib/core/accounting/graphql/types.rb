8d8be59be45efdb2f3061e9808b2f2221599fc88
adam <adam@moonlightlabs.com>
2023-04-10 13:54:43 -0700

    add graphql field and argument descriptions

--- core-accounting/lib/core/accounting/graphql/types.rb
+++ core-accounting/lib/core/accounting/graphql/types.rb
@@ -18,26 +18,26 @@ module Core
           # model reference:
           # https://github.com/envato/double_entry/blob/master/lib/double_entry/line.rb
 
-          field :account, String, null: false
-          field :amount, ::GraphQL::Types::JSON, null: false # money object
-          field :balance, ::GraphQL::Types::JSON, null: false # money object
-          field :code, String, null: false
-          field :detail_id, ID, null: true
-          field :detail_type, String, null: true
-          field :id, ID, null: false
-          field :metadata, ::GraphQL::Types::JSON, null: true
-          field :partner_account, String, null: false
-          field :partner_scope, String, null: true
-          field :scope, String, null: false
+          field :account, String, null: false, description: ''
+          field :amount, ::GraphQL::Types::JSON, null: false, description: '' # money object
+          field :balance, ::GraphQL::Types::JSON, null: false, description: '' # money object
+          field :code, String, null: false, description: ''
+          field :detail_id, ID, null: true, description: 'id of detail relation'
+          field :detail_type, String, null: true, description: 'class of detail relation'
+          field :id, ID, null: false, description: ::Core::Base::GraphQL::Documentation::Fields.id
+          field :metadata, ::GraphQL::Types::JSON, null: true, description: 'metadata of record'
+          field :partner_account, String, null: false, description: ''
+          field :partner_scope, String, null: true, description: ''
+          field :scope, String, null: false, description: ''
 
-          field :partner, ::Core::Accounting::GraphQL::Types::Line, null: true
+          field :partner, ::Core::Accounting::GraphQL::Types::Line, null: true, description: ''
 
-          field :created_at, ::GraphQL::Types::ISO8601DateTime, null: false
-          field :updated_at, ::GraphQL::Types::ISO8601DateTime, null: false
+          field :created_at, ::GraphQL::Types::ISO8601DateTime, null: false, description: ::Core::Base::GraphQL::Documentation::Fields.created_at
+          field :updated_at, ::GraphQL::Types::ISO8601DateTime, null: false, description: ::Core::Base::GraphQL::Documentation::Fields.updated_at
         end
 
         class LineListMetadata < BaseObject
-          field :count, Int, null: false
+          field :count, Int, null: false, description: ::Core::Base::GraphQL::Documentation::Fields.relation_count
         end
       end
     end
