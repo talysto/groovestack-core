be6b4c08d9ab2ef62519f3c175efbd765d5e15ff
Max Schridde <maxjschridde@gmail.com>
2023-04-14 11:59:26 -0700

    [core-base] graphql initializer transforms default_graphql_name to conform to ra data graphql resource naming conventions

--- core-base/config/initializers/graphql.rb
+++ core-base/config/initializers/graphql.rb
@@ -3,7 +3,11 @@ module GraphQL::Schema::Member::BaseDSLMethods
     @default_graphql_name ||= begin
       raise ::GraphQL::RequiredImplementationMissingError, 'Anonymous class should declare a `graphql_name`' if name.nil?
 
-      name.split("GraphQL").last.split("Types::").last.gsub("::", "")
+      # ex name: "GraphQL::User::Type"
+      # ex name: "GraphQL::Admin::User::Type"
+
+      graphql_name = name.split("GraphQL").last.split("Types::").last.split("::Type").first.gsub("::", "")
+      graphql_name.gsub("Admin", "") # assumption here that graphql schema def might have an admin namespace. TODO refactor
     end
   end
 end
