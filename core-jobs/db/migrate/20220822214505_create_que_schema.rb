b09f265e0323ecc1da9fcbc775678dce37e8df67
Max Schridde <maxjschridde@gmail.com>
2023-08-31 17:26:33 -0700

    [core-jobs] bump que dependency. bolster migrations

--- core-jobs/db/migrate/20220822214505_create_que_schema.rb
+++ core-jobs/db/migrate/20220822214505_create_que_schema.rb
@@ -5,11 +5,21 @@ class CreateQueSchema < ActiveRecord::Migration[6.0]
     # Whenever you use Que in a migration, always specify the version you're
     # migrating to. If you're unsure what the current version is, check the
     # changelog.
-    Que.migrate!(version: 6)
+
+    if Que.db_version == 0 # Que is not installed here so install the highest version
+      Que.migrate!(version: 7)
+      return
+    end
+
+    raise 'Que version must be updated to v5 before core-jobs can be installed' if Que.db_version < 5
+
+    Que.migrate!(version: 6) if Que.db_version == 5
+    Que.migrate!(version: 7) if Que.db_version == 6
   end
 
   def down
     # Migrate to version 0 to remove Que entirely.
+    
     Que.migrate!(version: 0)
   end
 end
