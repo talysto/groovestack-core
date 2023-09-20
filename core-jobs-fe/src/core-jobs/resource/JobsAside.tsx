123431ccfe3d094566d0ccb78ff10533f1dc7fd3
David <0davidcaplin@gmail.com>
2023-09-20 15:08:19 -0700

    comment clean up jobs

--- core-jobs-fe/src/core-jobs/resource/JobsAside.tsx
+++ core-jobs-fe/src/core-jobs/resource/JobsAside.tsx
@@ -13,37 +13,6 @@ const Item = styled(Paper)(({ theme }) => ({
 }))
 
 export const JobsAside = () => {
-  // const dataProvider = useDataProvider()
-  // const resource = useResourceContext()
-
-  // const refreshJobStatsTable = () => {
-  //   const resourcePath = resource.split('/')
-  //   const namespacedResource =
-  //     resourcePath.length > 1 ? `${resourcePath[0]}/JobReport` : 'JobReport'
-
-  //   return dataProvider.getList(namespacedResource, {
-  //     filter: { reportName: 'job_stats' },
-  //     pagination: { page: 0, perPage: 10 },
-  //     sort: { field: '', order: '' },
-  //   })
-  // }
-
-  // const refreshWorkersTable = () => {
-  //   const resourcePath = resource.split('/')
-  //   const namespacedResource =
-  //     resourcePath.length > 1 ? `${resourcePath[0]}/Locker` : 'Locker'
-
-  //   return dataProvider.getList(namespacedResource, {
-  //     filter: {},
-  //     pagination: { page: 1, perPage: 100 },
-  //     sort: { field: '', order: '' },
-  //   })
-  // }
-
-  // const transformJobStatsData = ({ data }: { data: any }) => {
-  //   return data[0].data
-  // }
-
   return (
     <Paper
       elevation={0}
@@ -56,30 +25,12 @@ export const JobsAside = () => {
       }}
     >
       <Stack spacing={3}>
-        {/* <Item elevation={0}>
-          <Stack
-              direction="row"
-              spacing={2}
-              sx={{ justifyContent: 'space-between', width: '100%' }}
-            >
-              <Box sx={{ flexBasis: '100%' }}><Metric value="14" label="Queued" units="k" /></Box>
-              <Box sx={{ flexBasis: '100%' }}><Metric value="32" label="Latency" units="min" /></Box>
-              <Box sx={{ flexBasis: '100%' }}><Metric value="4" label="Errors" /></Box>
-          </Stack>
-        </Item> */}
-
         <Item sx={{ height: 160 }} elevation={0}>
           <RPMChart />
         </Item>
-
         <Item sx={{ height: 200 }} elevation={0}>
           <UtilizationChart />
         </Item>
-
-        {/* <Item elevation={0}>
-          <JobsSummary />
-          <JobsSummaryPivot />
-        </Item> */}
       </Stack>
     </Paper>
   )
--- core-jobs-fe/src/core-jobs/resource/jobs/table.tsx
+++ core-jobs-fe/src/core-jobs/resource/jobs/table.tsx
@@ -1,10 +1,4 @@
-import {
-  Box,
-  Chip,
-  CircularProgress,
-  Stack,
-  Typography,
-} from '@mui/material'
+import { Box, Chip, CircularProgress, Stack, Typography } from '@mui/material'
 // import RunningWithErrorsIcon from '@mui/icons-material/RunningWithErrors'
 import AccessTimeIcon from '@mui/icons-material/AccessTime'
 import SummaryIcon from '@mui/icons-material/GridView'
@@ -39,35 +33,11 @@ import { JobsSummaryPivot } from '../../views/JobsSummary'
 import { ListViewToggleButtons } from './ListViewToggleButtons'
 
 const JobsFilters = [
-  // <FauxInput key="title" alwaysOn source="any">
-  //   <Typography variant="h3" sx={{ pr: 4 }}>
-  //     <span style={{fontWeight: 700}}>CORE</span><span style={{fontWeight: 200}}>Jobs</span>
-  //   </Typography>
-  // </FauxInput>,
   <SearchInput key="q" alwaysOn source="q" placeholder="Filter or search..." />,
-  // <SelectArrayInput key='s' alwaysOn source="status" choices={statuses.map((status) => (
-  //   {
-  //     id: status,
-  //     // name: <FormControlLabel control={<Checkbox defaultChecked />} label={status} />
-  //     name: <MenuItem>
-  //     <ListItemIcon>
-  //       <Check />
-  //     </ListItemIcon>
-  //     <ListItemText>{status}</ListItemText>
-  //     <Typography variant="body2" color="text.secondary">34</Typography>
-  //   </MenuItem>
-  //   }))} />
-  // <CheckboxGroupInput
-  //   key="status"
-  //   alwaysOn
-  //   source="status"
-  //   choices={jobStatuses}
-  // />,
 ]
 
 const enhancedStatus = (record: RaRecord) => {
   // var extended = null;
-
   const runningIcon = <CircularProgress size="0.75em" />
 
   const statusMap: { [key: string]: JSX.Element } = {
@@ -146,78 +116,9 @@ const sortfilterToggles = [
 
 const ListActions = () => (
   <TopToolbar>
-    {/* <>
-    <FilterList label="Status" icon={<Check />}>
-        <FilterListItem
-        label={<><Typography component='span'>Scheduled</Typography><Typography component='span' color="text.secondary">24,200</Typography></>}
-        value={{ has_newsletter: true }} />
-        <FilterListItem
-        label={<Box  sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}><Typography component='span'>Queued</Typography><Typography component='span' color="text.secondary">200</Typography></Box>}
-        value={{ status: ['scheduled'] }} />
-    </FilterList>
-  </> */}
     <ListViewToggleButtons sortfilterToggles={sortfilterToggles} />
-    {/* <ToggleButtonGroup size="small">
-      <ToggleButton value="summary" key="error">
-        <SummaryIcon fontSize="small" />
-        <SortFilterButton
-          label="Summary"
-          sortSpec={{ field: 'run_at', order: 'ASC' }}
-          filterSpec={{ view: 'summary' }}
-        />
-      </ToggleButton> */}
-    ,
-    {/* <ToggleButton defaultChecked value="list" key="all">
-        <ListIcon fontSize="small" />
-        <SortFilterButton
-          label="Reset"
-          sortSpec={{ field: 'run_at', order: 'ASC' }}
-          filterSpec={{}}
-        />
-      </ToggleButton>
-      ,
-      <ToggleButton value="errors" key="error">
-        <WarningAmberIcon fontSize="small" />
-        <SortFilterButton
-          label="Errors"
-          sortSpec={{ field: 'priority', order: 'ASC' }}
-          filterSpec={{ status: ['errored', 'failed'] }}
-        />
-      </ToggleButton>
-      ,
-      <ToggleButton value="scheduled" key="error">
-        <AccessTimeIcon fontSize="small" />
-        <span style={{ marginLeft: 4 }}>Scheduled</span>
-      </ToggleButton>
-    </ToggleButtonGroup> */}
-    {/* <SortFilterButton
-      label="Reset"
-      sortSpec={{ field: 'run_at', order: 'ASC' }}
-      filterSpec={{}}
-    /> */}
-    {/* <SortFilterButton label='14m Latency' sortSpec={{field: 'run_at', order: 'ASC'}} filterSpec={{status: ['queued']}} /> */}
-    {/* <SortFilterButton
-      label="Errors"
-      sortSpec={{ field: 'priority', order: 'ASC' }}
-      filterSpec={{ status: ['errored', 'failed'] }}
-    /> */}
-    {/* <SortFilterButton label='14,000 Scheduled' sortSpec={{field: 'run_at', order: 'DESC'}} filterSpec={{status: ['scheduled', 'queued']}} />  */}
-    {/* <Button href="?sort=run_at" label="14 Queued" onClick={() => {}} /> */}
-    {/* <Button label="32 min Latency"   onClick={() => {}} /> */}
-    {/* <FilterListItem
-        label={<Box  sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}><Typography component='span'>Queued</Typography><Typography component='span' color="text.secondary">200</Typography></Box>}
-        value={{ status: ['scheduled'] }} /> */}
-    {/* <MuiButton onClick={() => {}}>
-    <Typography component='div' sx={{fontWeight: 'bold'}}>14</Typography><Typography component='div' color="text.secondary">Errors</Typography>
-  </MuiButton> */}
-    {/* <Metric value="4" label="Errors" /> */}
-    {/* <FilterButton /> */}
-    {/* <Metric value="14" label="Queued" units="k" />
-<Metric value="32" label="Latency" units="min" />
-<Metric value="4" label="Errors" /> */}
   </TopToolbar>
 )
-
 // import * as style from "./show-on-hover.css"
 
 const JobActions = ({ label = 'Actions' }: { label?: string }) => {
