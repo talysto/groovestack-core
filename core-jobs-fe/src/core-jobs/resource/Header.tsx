494a97d370e56a7b1db096ca2bcaee2ccb800951
David <0davidcaplin@gmail.com>
2023-09-22 11:00:41 -0700

    WIP responsive jobs + fixes merge conflicts

--- core-jobs-fe/src/core-jobs/resource/Header.tsx
+++ core-jobs-fe/src/core-jobs/resource/Header.tsx
@@ -1,10 +1,13 @@
-import { Box, Stack, Typography } from '@mui/material';
-import { Metric } from '../components/Metric';
+import { Box, Grid, Typography, useMediaQuery, useTheme } from '@mui/material'
+import { Metric } from '../components/Metric'
 
-export const Header = () => (
-  <>
-    <style>
-      {`
+export const Header = () => {
+  const theme = useTheme()
+  const moreThanSmall = useMediaQuery(theme.breakpoints.up('sm'))
+  return (
+    <>
+      <style>
+        {`
 tr > td > .show-on-hover {
   visibility: hidden;
 }
@@ -13,14 +16,45 @@ tr:hover > td > .show-on-hover {
   visibility: visible;
 }
 `}
-    </style>
-    <Stack
-      direction="row"
+      </style>
+      <Grid container>
+        <Grid item xs={12} sm={8}>
+          <Typography variant="h4" sx={{}}>
+            <span style={{ fontWeight: 700 }}>CORE</span>
+            <span style={{ fontWeight: 200 }}>Jobs</span>
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Box
+            sx={{
+              display: 'flex',
+              gap: 2,
+              
+              ...(moreThanSmall ? { m: 2, px: 2 } : { mt: 2, mr: 2 })
+            }}
+          >
+            <Box sx={{ width: '100%' }}>
+              <Metric value="14" label="Queued" units="k" />
+            </Box>
+            <Box sx={{ width: '100%' }}>
+              <Metric value="32" label="Latency" units="min" />
+            </Box>
+            <Box sx={{ width: '100%' }}>
+              <Metric value="4" label="Errors" />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+
+      {/* <Stack
+      // direction="row"
+      direction={{ xs: 'column', md: 'row' }}
       sx={{
         display: 'flex',
         justifyContent: 'space-between',
         width: '100%',
-        alignItems: 'center',
+        // alignItems: 'center',
       }}
     >
       <Typography variant="h4" sx={{}}>
@@ -49,6 +83,7 @@ tr:hover > td > .show-on-hover {
           <Metric value="4" label="Errors" />
         </Box>
       </Stack>
-    </Stack>
-  </>
-);
+    </Stack> */}
+    </>
+  )
+}
