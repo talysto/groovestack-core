8b97ad1b64cc8cbdd4e7dbdad527c32db2b6b9f6
David <0davidcaplin@gmail.com>
2023-09-26 12:08:06 -0700

    temp edit jobs button

--- core-jobs-fe/src/core-jobs/resource/jobs/JobsDatagrid.tsx
+++ core-jobs-fe/src/core-jobs/resource/jobs/JobsDatagrid.tsx
@@ -44,6 +44,15 @@ export const JobDatagrid = () => {
       <JobStatusField source="status" />
 
       <TimeAgoField label="Scheduled" source="run_at" />
+      <CustomButtonDrawer
+        label="Edit"
+        drawerProps={{ title: 'Edit Jobs' }}
+        // sx={{ display: 'inline-flex' }}
+        mode="edit"
+        drawerWidth={DrawerWidth.Medium}
+      >
+        <EditJob />
+      </CustomButtonDrawer>
       <JobActions label="Actions" />
       {/* <CustomButtonDrawer
         label=" "
