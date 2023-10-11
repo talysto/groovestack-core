841b14bf1b35e161a7d71263ee0cbe73519608d5
Isom Durm <isomdurm@gmail.com>
2023-10-11 13:01:33 -0700

    remove unnecessary files

--- core-config-fe/src/core-config/resources/config/CustomLayout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Layout } from 'react-admin'
-import { CustomAppBar } from './CustomAppBar'
-
-export const CustomLayout = (props) => (
-  <Layout {...props} appBar={CustomAppBar} />
-)
