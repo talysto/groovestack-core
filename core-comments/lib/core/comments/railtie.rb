5997a10da3ddbcacc35ba705a3b903c4758ca2d3
Max Schridde <maxjschridde@gmail.com>
2023-10-27 12:49:47 -0700

    update railties for all versions

--- core-comments/lib/core/comments/railtie.rb
+++ core-comments/lib/core/comments/railtie.rb
@@ -4,45 +4,35 @@ if defined?(Rails)
   module Core
     module Comments
       class Railtie < Rails::Engine
-        # Enable our new migrations for the parent app
-        initializer :append_migrations do |app|
-          unless app.root.present? && root.present? && (app.root.to_s.match? root.to_s)
-            config.paths['db/migrate'].expanded.each do |expanded_path|
-              app.config.paths['db/migrate'] << expanded_path
-            end
-          end
+        include ::Core::Base::CoreRailtie
+
+        def dx_validations
+          [
+            {
+              eval: proc { raise unless defined?(::Core::Base) },
+              message: "Error: 'core-base' gem is required, add it your your gemfile"
+            },
+            {
+              eval: proc do
+                      unless Rails.application.config.active_record.schema_format == :sql
+                        raise Core::Comments::WrongSchemaFormat
+                      end
+                    end,
+              message: 'Error: Must change your schema format to :sql'
+            }
+          ]
         end
 
-        # Enable our new initializers for the parent app
+        initializer :append_migrations do |app|
+          append_migrations app
+        end
+
         initializer :append_initializers do |app|
-          unless app.root.present? && root.present? && (app.root.to_s.match? root.to_s)
-            config.paths['config/initializers'].expanded.each do |expanded_path|
-              app.config.paths['config/initializers'] << expanded_path
-            end
-          end
+          append_initializers app
         end
 
         config.after_initialize do
-          if (ENV['RAILS_ENV'] || ENV.fetch('RACK_ENV', nil)) == 'development'
-            validations = [
-              {
-                eval: proc { raise unless defined?(::Core::Base) },
-                message: "Error: 'core-base' gem is required, add it your your gemfile"
-              },
-              {
-                eval: proc do
-                        unless Rails.application.config.active_record.schema_format == :sql
-                          raise Core::Comments::WrongSchemaFormat
-                        end
-                      end,
-                message: 'Error: Must change your schema format to :sql'
-              }
-            ]
-
-            if defined?(::Core::Base) 
-              ::Core::Base::Railtie.core_base_dx_validate(validations, 'Comments') 
-            end
-          end
+          after_init
         end
       end
     end
--- core-referrals/lib/core/referrals/railtie.rb
+++ core-referrals/lib/core/referrals/railtie.rb
@@ -4,49 +4,39 @@ if defined?(Rails)
   module Core
     module Referrals
       class Railtie < Rails::Engine
-        # Enable our new migrations for the parent app
-        initializer :append_migrations do |app|
-          unless app.root.present? && root.present? && (app.root.to_s.match? root.to_s)
-            config.paths['db/migrate'].expanded.each do |expanded_path|
-              app.config.paths['db/migrate'] << expanded_path
-            end
-          end
+        include ::Core::Base::CoreRailtie
+
+        def dx_validations
+          [
+            {
+              eval: proc { raise unless defined?(::Core::Base) },
+              message: "Error: 'core-base' gem is required, add it your your gemfile"
+            },
+            {
+              eval: proc { require 'base32h' },
+              message: "Error: 'base32h' gem is required, add it your your gemfile"
+            },
+            {
+              eval: proc do
+                      unless Rails.application.config.active_record.schema_format == :sql
+                        raise Core::Referrals::WrongSchemaFormat
+                      end
+                    end,
+              message: 'Error: Must change your schema format to :sql'
+            }
+          ]
         end
 
-        # Enable our new initializers for the parent app
+        initializer :append_migrations do |app|
+          append_migrations app
+        end
+
         initializer :append_initializers do |app|
-          unless app.root.present? && root.present? && (app.root.to_s.match? root.to_s)
-            config.paths['config/initializers'].expanded.each do |expanded_path|
-              app.config.paths['config/initializers'] << expanded_path
-            end
-          end
+          append_initializers app
         end
 
         config.after_initialize do
-          if (ENV['RAILS_ENV'] || ENV.fetch('RACK_ENV', nil)) == 'development'
-            validations = [
-              {
-                eval: proc { raise unless defined?(::Core::Base) },
-                message: "Error: 'core-base' gem is required, add it your your gemfile"
-              },
-              {
-                eval: proc { require 'base32h' },
-                message: "Error: 'base32h' gem is required, add it your your gemfile"
-              },
-              {
-                eval: proc do
-                        unless Rails.application.config.active_record.schema_format == :sql
-                          raise Core::Referrals::WrongSchemaFormat
-                        end
-                      end,
-                message: 'Error: Must change your schema format to :sql'
-              }
-            ]
-
-            if defined?(::Core::Base) 
-              ::Core::Base::Railtie.core_base_dx_validate(validations, 'Referrals') 
-            end
-          end
+          after_init
         end
       end
     end
