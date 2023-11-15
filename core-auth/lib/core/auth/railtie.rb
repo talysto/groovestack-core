e1e194e955f6f8c3f10dd188404dde0a7f9231b6
Max Schridde <maxjschridde@gmail.com>
2023-11-14 17:12:40 -0800

    typo fix

--- core-auth/lib/core/auth/railtie.rb
+++ core-auth/lib/core/auth/railtie.rb
@@ -16,6 +16,7 @@ if defined?(Rails)
               eval: proc { raise unless defined?(::Core::Config) },
               message: "Error: 'core-config' gem is required, add it your your gemfile"
             },
+            {
               eval: proc { raise unless (app_config = ::Core::Config::App.generate_config) && ((app_config.keys.include?(:oauth_enabled) && !app_config[:oauth_enabled]) || ::Core::Auth::Providers::OmniAuth.enabled_providers.present? ) },
               message: "Warning: oauth is enabled but no providers are configured. Follow the instructions in the README to configure oauth providers or disable oauth in the groovestack initializer."
             }
