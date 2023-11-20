8b87363664cb8434d76b41fe0c6f54fbc1018235
Max Schridde <maxjschridde@gmail.com>
2023-11-20 10:55:00 -0800

    FIX core-auth core config initializer that prevents db create in launch template

--- core-auth/config/initializers/core_config.rb
+++ core-auth/config/initializers/core_config.rb
@@ -4,15 +4,20 @@ ActiveSupport.on_load(:after_initialize) do
     Core::Config::App.dynamic_config << { key: :user_roles, build: Proc.new { User::ROLES } }
 
     # if parent app disables oauth, render empty enabled array
-    app_config = Core::Config::App.generate_config
-    oauth_disabled = app_config.keys.include?(:oauth_enabled) && !app_config[:oauth_enabled]
+
+    # TODO: FIX can't access app_config here b/c initializers loaded on app load
+    # which is run for all rails commands. prevents db from getting created
+    
+    # app_config = Core::Config::App.generate_config
+    # oauth_disabled = app_config.keys.include?(:oauth_enabled) && !app_config[:oauth_enabled]
     
     Core::Config::App.dynamic_config << { 
       key: :oauth_providers, 
       build: Proc.new { 
         { 
           available: Core::Auth::Providers::OmniAuth.available_providers.map { |p| p.as_json[:k] }, 
-          enabled: oauth_disabled ? [] : Core::Auth::Providers::OmniAuth.enabled_providers.map { |p| p.as_json([:k, :path]) }
+          # enabled: oauth_disabled ? [] : Core::Auth::Providers::OmniAuth.enabled_providers.map { |p| p.as_json([:k, :path]) }
+          enabled: Core::Auth::Providers::OmniAuth.enabled_providers.map { |p| p.as_json([:k, :path]) }
         } 
       } 
     }
