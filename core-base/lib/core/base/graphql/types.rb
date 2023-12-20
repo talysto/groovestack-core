1ec973bfe2c101ade18f5f63cb16a32b9134b28f
Max Schridde <maxjschridde@gmail.com>
2023-12-20 14:56:31 -0800

    authorized base field / object

--- core-base/lib/core/base/graphql/types.rb
+++ core-base/lib/core/base/graphql/types.rb
@@ -26,6 +26,33 @@ module Core
           field_class ::Core::Base::GraphQL::Types::BaseField
         end
 
+        class AuthorizedField < BaseField
+          attr_accessor :authenticate
+  
+          def initialize(*args, association: false, **kwargs, &block)
+            kwargs[:resolver_method] = :resolve_association if association && defined?(Pundit) # ensure associations scopes are authorized
+  
+            super(*args, **kwargs, &block)
+          end
+  
+          def resolve_association
+            current_user = context[:current_user]
+            class_const = name.to_s.classify
+            policy = Pundit.policy!(current_user, class_const)
+            policy::IndexScope.new(current_user, class_const).resolve
+          end
+  
+          def authorized?(obj, args, ctx)
+            authorized_fields = obj.authorized_fields_for_serialization(ctx[:current_user]) rescue []
+  
+            super && authorized_fields.include?(name.to_sym)
+          end
+        end
+  
+        class AuthorizedObject < BaseObject
+          field_class AuthorizedField
+        end
+
         class SubscriptionPayload < ::Core::Base::GraphQL::Types::BaseObject
           field :subscription, String, null: false
           field :event, ::GraphQL::Types::JSON, null: false
