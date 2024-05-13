d1a3baf9942b2cda8d275e5f19d56953c72ac5d1
David <0davidcaplin@gmail.com>
2024-05-13 15:18:28 -0700

    RatingField Storybook basic docs and Story

--- labs/core-labs-fe/src/react-admin/inputs/RatingField/RatingField.tsx
+++ labs/core-labs-fe/src/react-admin/inputs/RatingField/RatingField.tsx
@@ -1,14 +1,24 @@
 import { Rating, RatingProps } from "@mui/material"
-import { FieldProps } from "react-admin"
+import { FieldProps, useRecordContext } from "react-admin"
 
 export interface RatingFieldProps extends FieldProps {
   componentProps?: RatingProps
 }
 
+/**
+ * RatingField is a React Admin field that formats a rating value as a MUI rating component.
+ *
+ * FEATURES
+ * - Standard React Admin Field property interface and the ability to pass MUI Rating component props via 'componentProps'
+ *
+ * TODO
+ * - test further
+ *
+ * NOTES
+ * - Rating field show what others think of something.
+ */
 export const RatingField = ({componentProps, ...props}: RatingFieldProps) => {
-  //  const record = useRecordContext(props);
-
-  // const { record, source } = props
-  // return <Rating value={record[source]} {...componentProps} />
-  return <Rating {...componentProps} {...props} />
+  const record = useRecordContext(props);
+  const value = props.source ? record[props.source] : undefined;
+  return <Rating value={value} {...componentProps} readOnly={true} />
 }
