8fbe4acca4ebc85fd7d27ae008b68026d270fe03
David <0davidcaplin@gmail.com>
2024-05-30 16:53:33 -0700

    WIP launch button

--- labs/core-labs-fe/src/react-admin/socialLink/SocialLinkInput/SocialLinkInput.tsx
+++ labs/core-labs-fe/src/react-admin/socialLink/SocialLinkInput/SocialLinkInput.tsx
@@ -1,11 +1,11 @@
-import { InputProps, TextInput, useInput } from 'react-admin';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import LaunchIcon from '@mui/icons-material/Launch';
-import { Box, Stack } from '@mui/material';
-import React from 'react';
+import FacebookIcon from '@mui/icons-material/Facebook'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import LaunchIcon from '@mui/icons-material/Launch'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import { Box, Stack } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { InputProps, TextInput, useInput, useRecordContext } from 'react-admin'
 
 export enum SocialMediaPlatform {
   Instagram = 'Instagram',
@@ -18,10 +18,10 @@ const SocialMediaIcons = {
   [SocialMediaPlatform.Facebook]: FacebookIcon,
   [SocialMediaPlatform.Twitter]: TwitterIcon,
   [SocialMediaPlatform.LinkedIn]: LinkedInIcon,
-};
+}
 
 export interface SocialLinkProps extends InputProps {
-  platform: SocialMediaPlatform;
+  platform: SocialMediaPlatform
 }
 
 /**
@@ -34,25 +34,38 @@ export interface SocialLinkProps extends InputProps {
  * NOTES
  * - Rating Input allows people to share their opinions on something.
  */
-export const SocialLinkInput = ({
-  platform,
-  ...props
-}: SocialLinkProps) => {
-  const { field } = useInput(props);
-  const IconComponent = SocialMediaIcons[platform];
+export const SocialLinkInput = ({ platform, ...props }: SocialLinkProps) => {
+  // const { field } = useInput(props)
+  const IconComponent = SocialMediaIcons[platform]
+  const record = useRecordContext()
+  const [link, setLink] = useState(record.instagram_url) //TODO use lodash to drill down and get the correct platform on intial load
+
+  useEffect(() => {
+    console.log('link', link)
+  }, [link])
 
   if (!IconComponent) {
-    console.error(`No icon found for platform: ${platform}`);
-    return null;
+    console.error(`No platform set: ${platform}`)
+    return null
   }
 
   return (
-    <Stack direction='row' alignItems='center'>
-      <Box sx={{ pb: 1, pr: 1 }}>
-        <IconComponent fontSize='large' />
+    <Stack direction="row" alignItems="center">
+      <Box sx={{ pb: 1.5, pr: 1 }}>
+        <IconComponent fontSize="large" />
       </Box>
-      <TextInput {...props} />
-      <LaunchIcon/>
+      <TextInput
+        {...props}
+        variant="outlined"
+        onChange={(e) =>
+          setLink(e.target.value)
+        }
+      />
+      <Box sx={{ pb: 1.5, pl: 1 }}>
+        <LaunchIcon fontSize="small"
+        onClick={() => window.open(link, '_blank')}
+        />
+      </Box>
     </Stack>
-  );
-};
+  )
+}
